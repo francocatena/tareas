9ebfe769964a9cc783c425d853d0d23e1df3b164
Franco Catena <francocatena@gmail.com>
2011-10-24 12:02:44 -0200

    Path para "ayudar" a cron a encontrarse con Ruby =)

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,9 @@
+# Para actualizar la tabla de cron de desarrollo:
+# whenever --set environment=development --update-crontab tareas
+# Para eliminarla
+# whenever -c tareas
+env :PATH, '"/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"'
+
 every 1.day, at: '20:00' do
   runner 'Tarea.recordar_vencimientos'
 end
